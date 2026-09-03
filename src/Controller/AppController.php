26d30da7786e5c9cb90a7d1619e7450fd1053ec9
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2026-09-03 15:31:18 +0200

    PB-54504 - Fix MissingTemplateException on .json endpoints when X-Requested-With: XMLHttpRequest is set

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -92,7 +92,7 @@ class AppController extends Controller
         parent::beforeRender($event);
 
         // Required to support automatic view switching for 'ajax', which was supported by deprecated RequestHandlerComponent
-        if ($this->request->is('ajax')) {
+        if ($this->request->is('ajax') && !$this->request->is('json')) {
             $this->viewBuilder()->setClassName('Ajax');
         }
     }
